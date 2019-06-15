b13924598bb06fbe91c7dbe49ae5136b45def9bc
Yuki.Sasaki <lostheaven0222@ktj.biglobe.ne.jp>
2019-06-15 11:39:56 +0000

    Updated Top page

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,13 @@
 |
 */
 
-Route::get('/', 'TasksController@index');
+Route::get('/', function () {
+    return view('welcome');
+});
 
-Route::resource('tasks', 'TasksController'); // 'tasks'でURLを生成しているのか？？
+Route::resource('tasks', 'TasksController'); // tasks の部分がURLになる
+
+// ログイン認証を追加
+Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
+Route::post('login', 'Auth\LoginController@login')->name('login.post');
+Route::get('logout', 'Auth\LoginController@logout')->name('logout.get');
